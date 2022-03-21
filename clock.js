5a779d449b3f5f7e44103fd2148acfe3e0d9c746
tthugy <supermanace7@gmail.com>
2022-03-22 07:15:39 +0900

    Timeouts and Dates

--- clock.js
+++ clock.js
@@ -1,7 +1,10 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date();
+
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-// setInterval(sayHello, 2000);
+getClock();
+setInterval(getClock, 1000); 
