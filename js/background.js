2040618c811ebf09117d856655663b3eb57d33e1
tthugy <supermanace7@gmail.com>
2022-03-30 08:15:05 +0900

    Css 1

--- js/background.js
+++ js/background.js
@@ -2,8 +2,10 @@ const images = [ "bg01.jpeg", "bg02.jpeg", "bg03.jpeg" ];
 
 const selectImg = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const bgImage = document.querySelector(".inner_cont img");
 
 bgImage.src = `img/${selectImg}`;
 
-document.body.appendChild(bgImage);
+bgImage.classList.add("bgImage");
+
+// innerCont.appendChild(bgImage);
