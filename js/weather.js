112f1187bcb6b833b651712969645fc786d4d5f0
tthugy <supermanace7@gmail.com>
2022-03-30 23:36:01 +0900

    Css 2

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,21 @@
-const API_KEY = "dbdeb87d53226daa0f94775094b4e582";
+// const API_KEY = "dbdeb87d53226daa0f94775094b4e582";
 
-function onGeoOk(position){
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const city = document.querySelector("#weather span:first-child");
-        const weather = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-      });
-}
+// function onGeoOk(position){
+//     const lat = position.coords.latitude;
+//     const lon = position.coords.longitude;
+//     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(data => {
+//         const city = document.querySelector("#weather span:first-child");
+//         const weather = document.querySelector("#weather span:last-child");
+//         city.innerText = data.name;
+//         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+//       });
+// }
 
-function onGeoError(){
-    alert("Can't find you. No weather for you.");
-}
+// function onGeoError(){
+//     alert("Can't find you. No weather for you.");
+// }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+// navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
